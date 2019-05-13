82ef0cb88344a168c8a3c0aaaccfae247d0baf8b
Erik Yo <erikyo83@gmail.com>
2019-05-13 15:09:23 +0200

    better block animations + template footer and header

--- footer.php
+++ footer.php
@@ -1,20 +1,6 @@
   </div><!-- /content -->
 
-  <footer id="colophon" class="site-footer">
-
-	  <?php if(is_active_sidebar('footer-main')): ?>
-        <div class="footer-widgets main-width alignwide">
-		        <?php dynamic_sidebar('footer-main'); ?>
-        </div>
-    <?php endif; ?>
-
-    <div class="footer-credits">
-      <p class="main-width alignwide text-right">
-        Proudly powered by WordPress - © <?php the_date('Y')?> <?php echo str_replace(array( 'http://', 'https://' ), '', site_url()); ?> - Made with ♥ by codekraft-studio.
-      </p>
-    </div>
-
-    </footer><!-- /colophon -->
+  <?php get_template_part( 'template-parts/footer/footer' ); ?>
 
 </div><!-- /page -->
 
--- header.php
+++ header.php
@@ -8,43 +8,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-  <header id="masthead" class="site-header">
 
-    <div class="site-branding-container main-width alignwide">
-
-      <div class="site-branding">
-          <?php if ( has_custom_logo() ) : ?>
-              <div class="site-logo"><?php the_custom_logo(); ?></div>
-          <?php else : ?>
-              <h2 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h2>
-              <p class="site-description"><?php echo get_bloginfo( 'description' ); ?></p>
-          <?php endif; ?>
-      </div>
-
-
-      <?php if ( has_nav_menu( 'header-main' ) ) : ?>
-          <nav id="site-navigation" class="main-navigation">
-              <?php
-              wp_nav_menu(
-                  array(
-                      'theme_location' => 'header-main',
-                      'menu_class'     => 'main-menu',
-                      'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                  )
-              );
-              ?>
-          </nav>
-      <?php endif; ?>
-
-      <div class="menu-shadow" onclick="document.querySelector('#masthead').classList.toggle('active')"></div>
-      <div class="menu-resp unselectable" onclick="document.querySelector('#masthead').classList.toggle('active')">
-        <button class="c-hamburger">
-          <span>Toggle</span>
-        </button>
-      </div>
-
-    </div>
-
-  </header>
+  <?php get_template_part( 'template-parts/header/header' ); ?>
 
   <div id="content" class="site-content">
