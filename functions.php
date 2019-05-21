351326295dc6dbcab177f82d4dbefd04db93bc55
Erik Yo <erikyo83@gmail.com>
2019-05-21 20:46:27 +0200

    removal of clean.php for compliance with wordpress theme guidelines

--- functions.php
+++ functions.php
@@ -1,7 +1,5 @@
 <?php
 
-// require_once( get_template_directory() . '/inc/clean.php' );
-
 require_once( get_template_directory() . '/inc/theme-setup.php' );
 require_once( get_template_directory() . '/inc/sidebar.php' );
 require_once( get_template_directory() . '/inc/enqueue-scripts.php' );
