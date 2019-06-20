e8c26d6c9c4eeab3346ee0bd0c831a459cbef2fd
Erik Yo <erikyo83@gmail.com>
2019-06-20 14:19:37 +0200

    homepage headline regression fix

--- template-parts/content/content-homepage.php
+++ template-parts/content/content-homepage.php
@@ -8,7 +8,7 @@
 <div class="website-hero text-center">
 	<?php modul_r_post_image('parallax'); ?>
   <div class="hero-title text-center">
-    <h1><?php bloginfo( 'name' ); ?></h1>
+    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
     <p><?php bloginfo('description'); ?></p>
     <a href="https://codekraft.it" class="button big"><?php _e('Contact us',  'modul-r'); ?></a>
     <a href="https://github.com/erikyo/Modul-R" class="button big outline" ><?php _e('Download',  'modul-r'); ?></a>
@@ -16,11 +16,6 @@
 </div>
 
 <div class="entry-content">
-
-  <header class="entry-header main-width">
-    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-  </header>
-
   <?php the_content(); ?>
 </div><!-- /entry-content -->
 
