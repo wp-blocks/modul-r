158c312752120dbda68ca3e4afff01f2999f762d
Erik Yo <erikyo83@gmail.com>
2019-06-28 14:39:10 +0200

    woocommerce category widget accordion

--- assets/src/js/user/index.js
+++ assets/src/js/user/index.js
@@ -78,3 +78,45 @@ jQuery(document).ready(function($){
 
 
 });
+
+
+// WooCommerce category accordion
+jQuery(document).ready(function($) {
+
+  if ($('ul.product-categories').length > 0) {
+
+    $('.product-categories li.cat-parent > a').prepend('<span class="toggle"><i class="material-icons">arrow_forward</i></span>');
+    $('.product-categories .children').hide();
+    $('.product-categories li.current-cat-parent > .children, .product-categories li.current-cat > .children').show();
+    $('.product-categories li.current-cat, .product-categories li.current-cat-parent').addClass('active');
+
+    $(function () {
+
+      $('.product-categories').find('a').on('click', function (e) {
+
+        const catItem = $(this).parent('.cat-item');
+
+        if ( ! catItem.hasClass('active')) {
+
+          catItem.addClass('active');
+
+          if ( catItem.hasClass('cat-parent') || catItem.hasClass('current-cat')) {
+            e.preventDefault();
+          }
+
+          $(this).parents('.product-categories > li').siblings().removeClass('active');
+
+          $(this).siblings('.children').stop(true, true).slideToggle()
+            .parents('.cat-item').siblings().children('.children').stop(true, true).slideUp();
+
+        }
+
+      });
+
+    });
+
+  }
+
+});
+
+
--- functions.php
+++ functions.php
@@ -8,3 +8,18 @@ require_once( get_template_directory() . '/inc/enqueue-scripts.php' );
 require_once( get_template_directory() . '/inc/mimetypes.php' );
 require_once( get_template_directory() . '/inc/template-functions.php' );
 require_once( get_template_directory() . '/inc/custom.php' );
+
+
+
+
+function woocommerce_after_shop_loop_item_title_short_description() {
+	global $product;
+	$short_desc = $product->get_short_description();
+	if ( $short_desc == "" ) return;
+	?>
+	<div class="woocommerce-loop-description" itemprop="description">
+		<?php echo apply_filters( 'woocommerce_short_description', $short_desc ) ?>
+	</div>
+	<?php
+}
+add_action('woocommerce_after_shop_loop_item_title', 'woocommerce_after_shop_loop_item_title_short_description', 5);
