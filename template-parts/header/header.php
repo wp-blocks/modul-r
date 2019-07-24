51e446c62f2f77202b73e6a848a76bf830a3816e
Erik Yo <erikyo83@gmail.com>
2019-07-24 22:20:31 +0200

    accessibility: header fixes

--- template-parts/header/header.php
+++ template-parts/header/header.php
@@ -14,11 +14,11 @@
       <div class="site-logo"><?php the_custom_logo(); ?></div>
       <?php if ( $show_header_text ) : ?>
         <div class="header-text">
-          <h2 class="site-title">
+          <h1 class="site-title">
             <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo _wp_specialchars( get_bloginfo( 'name' ), 1 ) ?>" rel="home" class="primary-color">
 		        <?php bloginfo( 'name' ); ?>
             </a>
-          </h2>
+          </h1>
           <?php if ( $description ) : ?>
             <p class="site-description"><?php echo $description; ?></p>
           <?php endif; ?>
