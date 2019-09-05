f923536d7352d3c955d2c18ca26e7333af507d98
Erik Yo <erikyo83@gmail.com>
2019-09-05 17:54:15 +0200

    scrollcontrol.js fix top distance when the adminbar is showed

--- assets/src/js/user/scrollControl.js
+++ assets/src/js/user/scrollControl.js
@@ -1,22 +1,23 @@
 "use strict";
 
-var parallaxDefaultSpeed = 0.3;
-var shiftView = 200; // make the elements triggered before fully in the viewport
+const parallaxDefaultSpeed = 0.3;
+const shiftView = 200; // make the elements triggered before fully in the viewport
 
-var vScrollTop = 0;
-var lastScroll = 0;
-var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-var headerHeight = document.getElementById('masthead').clientHeight;
-var scrollOffset = headerHeight; // the screen height where the header resize was triggered
+let vScrollTop = 0;
+let lastScroll = 0;
+const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+let headerHeight = document.getElementById('masthead').clientHeight;
+let scrollOffset = headerHeight; // the screen height where the header resize was triggered
+const headerDistanceFromTop = document.getElementById("masthead").offsetTop;
 
-var isInViewport = function (elem) {
-  var rect = elem.getBoundingClientRect();
+let isInViewport = function (elem) {
+  let rect = elem.getBoundingClientRect();
   return (!(rect.bottom < 0 || rect.top - viewHeight >= 0));
 };
 
-var isFullyVisible = function (elem) {
-  var rect = elem.getBoundingClientRect();
-  var rectBottom;
+let isFullyVisible = function (elem) {
+  let rect = elem.getBoundingClientRect();
+  let rectBottom;
   (rect.bottom - rect.top > viewHeight ) ? rectBottom = rect.top + viewHeight : rectBottom = rect.bottom ;
   return (rect.top + shiftView >= 0) && (rectBottom - shiftView <= document.documentElement.clientHeight);
 };
@@ -24,8 +25,8 @@ var isFullyVisible = function (elem) {
 
 function VisibleItemsTrigger(top) {
 
-  var animated = document.getElementsByClassName("interactive");
-  var arr = [].slice.call(animated);
+  let animated = document.getElementsByClassName("interactive");
+  let arr = [].slice.call(animated);
 
   arr.forEach(function(entry) {
 
@@ -41,8 +42,8 @@ function VisibleItemsTrigger(top) {
       // Parallax images
       // check if interactive box have a img child and get the distance from top
       if (entry.classList.contains('parallax')) {
-        var firstTop = window.pageYOffset + entry.getBoundingClientRect().top + scrollOffset;
-        var moveTopItem = -(firstTop - top) * parallaxDefaultSpeed;
+        let firstTop = window.pageYOffset + entry.getBoundingClientRect().top - headerDistanceFromTop;
+        let moveTopItem = -(firstTop - top) * parallaxDefaultSpeed;
         entry.getElementsByTagName('img')[0].style.transform = "translateY(" + moveTopItem + "px)";
       }
     }
@@ -51,7 +52,7 @@ function VisibleItemsTrigger(top) {
 
 
 function throttle(fn, wait) {
-  var time = Date.now();
+  let time = Date.now();
   return function() {
     if ((time + wait - Date.now()) < 0) {
       fn();
