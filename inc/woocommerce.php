3453ee03f1a6435102e33cc5bf149126459e67a6
Erik Yo <erikyo83@gmail.com>
2021-12-01 03:12:57 +0100

    split woocommerce style into a separate stylesheet

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -2,6 +2,15 @@
 
 if ( class_exists( 'WooCommerce' ) ) {
 
+    /**
+     * Enqueue style
+     */
+    function modul_r_woo_style() {
+        wp_register_style( 'modul-r-woo-css', get_template_directory_uri() . '/assets/dist/css/woo.css' );
+        wp_enqueue_style( 'modul-r-woo-css' );
+    }
+    add_action( 'wp_print_styles', 'modul_r_woo_style' );
+
 	function modul_r_add_woocommerce_support() {
 		add_theme_support( 'woocommerce' );
 	}
@@ -10,5 +19,4 @@ if ( class_exists( 'WooCommerce' ) ) {
 	add_theme_support( 'wc-product-gallery-zoom' );
 	add_theme_support( 'wc-product-gallery-lightbox' );
 	add_theme_support( 'wc-product-gallery-slider' );
-
 }
