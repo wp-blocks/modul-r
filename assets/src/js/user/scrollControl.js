8b6468f896bbadfdaa9f05bda926a780a34580b1
Erik Yo <erikyo83@gmail.com>
2021-12-04 14:35:18 +0100

    Scroll control script enhanced (higher framerate + code readability)

--- assets/src/js/user/scrollControl.js
+++ assets/src/js/user/scrollControl.js
@@ -10,6 +10,16 @@ let headerHeight = document.getElementById('masthead').clientHeight;
 let scrollOffset = headerHeight; // the screen height where the header resize was triggered
 const headerDistanceFromTop = document.getElementById("masthead").offsetTop;
 
+function throttle(fn, wait) {
+  let time = Date.now();
+  return function() {
+    if ((time + wait - Date.now()) < 0) {
+      fn();
+      time = Date.now();
+    }
+  }
+}
+
 let isInViewport = function (elem) {
   let rect = elem.getBoundingClientRect();
   return (!(rect.bottom < 0 || rect.top - viewHeight >= 0));
@@ -30,6 +40,7 @@ function VisibleItemsTrigger(top) {
 
   arr.forEach(function(entry) {
 
+    // this will add classes to the item to describe visibility
     if (isFullyVisible(entry)) {
       entry.classList.add('visible');
       entry.classList.add('already-see');
@@ -54,36 +65,22 @@ function VisibleItemsTrigger(top) {
   });
 }
 
-
-function throttle(fn, wait) {
-  let time = Date.now();
-  return function() {
-    if ((time + wait - Date.now()) < 0) {
-      fn();
-      time = Date.now();
-    }
-  }
-}
-
 function scrollCallback() {
-
   vScrollTop = window.pageYOffset;
   headerHeight = document.getElementById('masthead').clientHeight;
 
-  if ( lastScroll > vScrollTop ) {
-    document.body.classList.remove('scrolled');
-  } else {
-    vScrollTop > scrollOffset ? document.body.classList.add('scrolled') : document.body.classList.remove('scrolled');
-  }
+  // add/remove the ".top" class to masthead after scrolling down
+  ( lastScroll > vScrollTop ) ? document.body.classList.remove('scrolled') :
+    vScrollTop > scrollOffset ? document.body.classList.add('scrolled') :
+      document.body.classList.remove('scrolled');
 
-  if (vScrollTop < 5) {
-    document.body.classList.add('top');
-  } else {
-    document.body.classList.remove('top');
-  }
+  // add/remove the ".top" class to masthead near the top of the page
+  ( vScrollTop < 5 ) ? document.body.classList.add('top') : document.body.classList.remove('top');
 
+  // check for items visibility
   VisibleItemsTrigger(vScrollTop);
 
+  // store the vertical scroll value
   lastScroll = vScrollTop;
 }
 
@@ -91,7 +88,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   scrollCallback();
 
-  window.addEventListener('scroll', throttle(scrollCallback, 5), true); // 1 time every 5ms
+  window.addEventListener('scroll', throttle(scrollCallback, 2), true); // 1 time every 5ms
   window.addEventListener('resize', scrollCallback, true);
 
 });
