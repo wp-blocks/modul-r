abc4132ff72ef122d4e2f6151408887a33d04486
Erik Yo <erikyo83@gmail.com>
2021-12-04 14:33:58 +0100

    reworked menu script (jQuery removed)

--- assets/src/js/user/menu.js
+++ assets/src/js/user/menu.js
@@ -1,15 +1,27 @@
-// Change 'hover' to 'click' if you want to
-jQuery(document).ready(function($){
+/* *
+* Menu Scripts
+* */
+document.addEventListener("DOMContentLoaded", function(event) {
 
-  $('nav li > .sub-menu').parent().hover(function () {
+  const menu = document.getElementById("site-navigation"),
+        menuItems = menu.querySelectorAll("nav li");
 
-    var submenu = $(this).children('.sub-menu');
+  const handleMenuOn = (event) => {
+    const subMenu = event.currentTarget.querySelector(".sub-menu");
+    if (subMenu) subMenu.classList.add('active');
+  };
 
-    if (!$(submenu).hasClass("active")) {
-      $(submenu).addClass("active");
-    } else {
-      $(submenu).removeClass("active");
-    }
+  const handleMenuOff = (event) => {
+    const subMenu = event.currentTarget.querySelector(".sub-menu");
+    if (subMenu) subMenu.classList.remove('active');
+  };
+
+  menuItems.forEach(item => {
+
+    // Change 'click' || 'mouseover' if you want to
+    // item.addEventListener('click', handleMenuClick );
+    item.addEventListener('mouseover', handleMenuOn );
+    item.addEventListener('mouseout', handleMenuOff );
 
   });
 });
