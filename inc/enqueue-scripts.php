cdc94527ccce25dea6aeae9400a85f5044c95c70
Erik Yo <erikyo83@gmail.com>
2021-12-06 01:16:02 +0100

    lazyload animation fade-in support

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -135,9 +135,12 @@ endif;
 add_action( 'wp_enqueue_scripts', 'modul_r_theme_scripts' ); // Add Theme admin scripts
 
 /**
- * this ensures that images that are loaded with lazyload (from autoptimize) have the fade effect
+ * this ensures that images that are loaded with lazyload (from autoptimize) have a fancy fade effect
  */
-add_filter( 'autoptimize_filter_imgopt_lazyload_cssoutput' , '<style>[data-src]{filter: opacity(0);}img.lazyloaded{animation: lazyFadeIn linear .2s;filter: opacity(1);}@keyframes lazyFadeIn{0%{filter: opacity(0);}100%{filter: opacity(1);}}</style>' );
+add_filter( 'autoptimize_filter_imgopt_lazyload_cssoutput' , 'modul_r_lazyload_output' );
+function modul_r_lazyload_output() {
+   return '<style>[data-src]{filter: opacity(0);}img.lazyloaded{animation: lazyFadeIn linear .2s;filter: opacity(1);}@keyframes lazyFadeIn{0%{filter: opacity(0);}100%{filter: opacity(1);}}</style>';
+}
 
 
 /**
