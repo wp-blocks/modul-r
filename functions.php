c185f760c5ac7cfbbb4591925331531e49b384fe
Erik Golinelli <erik@codekraft.it>
2022-12-05 13:00:06 +0100

    restore the theme generated palette

--- functions.php
+++ functions.php
@@ -94,3 +94,4 @@ require_once( get_template_directory() . '/inc/sidebar.php' );
 require_once( get_template_directory() . '/inc/masonry.php' );
 require_once( get_template_directory() . '/inc/enqueue-scripts.php' );
 require_once( get_template_directory() . '/inc/template-functions.php' );
+require_once( get_template_directory() . '/inc/block-patterns.php' );
