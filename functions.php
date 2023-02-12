361ad6afc1ba579f866f9c0c874c9d8036446897
Erik Golinelli <erik@codekraft.it>
2023-02-12 14:16:51 +0100

    removes unused functions

--- functions.php
+++ functions.php
@@ -52,7 +52,6 @@ require_once( get_template_directory() . '/inc/theme-setup.php' );
 require_once( get_template_directory() . '/inc/customizer.php' );
 require_once( get_template_directory() . '/inc/woocommerce.php' );
 require_once( get_template_directory() . '/inc/sidebar.php' );
-require_once( get_template_directory() . '/inc/masonry.php' );
 require_once( get_template_directory() . '/inc/enqueue-style.php' );
 require_once( get_template_directory() . '/inc/enqueue-scripts.php' );
 require_once( get_template_directory() . '/inc/template-functions.php' );
--- inc/template-functions.php
+++ inc/template-functions.php
@@ -1,215 +1,56 @@
 <?php
-/**
- * Displays page-links for paginated posts
- */
-if ( ! function_exists( 'modul_r_page_links' ) ) :
-	function modul_r_page_links() {
-
-	  $args = array(
-		  'before'           => '<p>' . esc_html__( 'Pages:',  'modul-r' ),
-		  'after'            => '</p>',
-		  'link_before'      => '',
-		  'link_after'       => '',
-		  'next_or_number'   => 'number',
-		  'separator'        => ' ',
-		  'nextpagelink'     => esc_html__( 'Next page',  'modul-r'),
-		  'previouspagelink' => esc_html__( 'Previous page',  'modul-r' ),
-		  'pagelink'         => '%'
-	  );
-
-	  $pagination = wp_link_pages( $args );
-
-	  return $pagination;
-  }
-endif;
-
-
-/**
- * Displays the comments template
- */
 if ( ! function_exists('modul_r_comments') ) :
+	/**
+	 * Displays the comments template
+	 *
+	 * @return void
+	 */
 	function modul_r_comments() {
-		// If comments are open or we have at least one comment, load up the comment template.
+		// If comments are open, or we have at least one comment, load up the comment template.
 		comments_template();
 	}
 endif;
 
-
-/**
- * Displays the tags
- */
-if ( ! function_exists('modul_r_tags') ) :
-	function modul_r_tags() {
-
-		if( has_tag() ): ?>
-			<div class="post-tags">
-				<h3><?php esc_html_e('Tags:',  'modul-r'); ?></h3>
-				<ul><?php the_tags( '<li class="post-tag">', '</li><li class="post-tag">', '</li>');  ?></ul>
-			</div>
-		<?php endif;
-
-	}
-endif;
-
-/**
- * Displays the article meta (author / date / comments number if isn't zero)
- */
-if ( ! function_exists('modul_r_meta') ) :
-	function modul_r_meta() {
-
-    global $post;
-    $post_comments = get_comment_count($post->ID);
-	  $approved = $post_comments['approved'];
-
-		?>
-    <div class="post-meta">
-
-      <a href="<?php the_permalink(); ?>" rel="bookmark" class="hide"><?php the_title(); ?></a>
-
-      <div class="meta-wrapper">
-
-        <p>
-          <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>" rel="author" class="h-card url" >
-            <span class="author fn"><?php the_author_meta( 'display_name' ); ?></span>
-          </a>
-        </p>
-
-        <p><b> | </b></p>
-
-        <p>
-          <a href="<?php echo get_day_link( get_the_time('Y'), get_the_time('m'), get_the_time('d') ); ?>">
-
-            <?php if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) : ?>
-              <time class="entry-date published updated hide" datetime="<?php echo esc_attr( get_the_date( DATE_W3C ) ) ?>"><?php echo esc_html( get_the_date() ); ?></time>
-	          <?php else : ?>
-              <time class="entry-date published hide" datetime="<?php echo esc_attr( get_the_date( DATE_W3C ) ); ?>"><?php echo esc_html( get_the_date() ); ?></time>
-              <time class="updated hide" datetime="<?php echo esc_attr( get_the_modified_date( DATE_W3C ) ); ?>"><?php echo esc_html( get_the_modified_date() ); ?></time>
-		        <?php endif; ?>
-
-	          <span><?php echo apply_filters( 'the_date', get_the_date(), get_option( 'date_format' ), '', '' ); ?></span>
-
-          </a>
-        </p>
-
-        <?php if ( $approved > 0 ) { ?>
-
-        <p><b> | </b></p>
-
-        <p>
-          <a href="<?php the_permalink(); ?>#comments">
-            <?php
-            /* translators: %s: the number of comments */
-            printf( _n( '%s comment', '%s comments', $approved, 'modul-r' ), $approved );
-            ?>
-          </a>
-        </p>
-
-        <?php } ?>
-
-      </div>
-    </div>
-
-		<?php
-
-	}
-endif;
-
-
-/**
- * Displays the article breadcrumbs
- */
 if ( ! function_exists('modul_r_breadcrumbs') ) :
+	/**
+	 * Displays the article breadcrumbs
+	 *
+	 * @return void
+	 */
 	function modul_r_breadcrumbs() {
 	  if ( function_exists('yoast_breadcrumb') ) {
 		  yoast_breadcrumb( '<p class="breadcrumbs">','</p>' );
 	  } else {
 	    if (is_single()) {
-	      printf('<p class="breadcrumbs"><a href="%s">%s</a> / %s</p>', esc_url(home_url()) , esc_html__('Home', 'modul-r'), get_the_category_list(' &#47; ') );
+	      printf('<p class="breadcrumbs"><a href="%s">%s</a> / %s</p>', esc_url(home_url()) , esc_html__('Home', 'modul-r'), esc_html(get_the_category_list(' &#47; ') ) );
       } else {
-	      printf('<p class="breadcrumbs"><a href="%s">%s</a> / <a href="%s">%s</a></p>', esc_url(home_url()) , esc_html__('Home', 'modul-r'), get_permalink(), get_the_title() );
+	      printf('<p class="breadcrumbs"><a href="%s">%s</a> / <a href="%s">%s</a></p>', esc_url(home_url()) , esc_html__('Home', 'modul-r'), esc_url(get_permalink()), esc_html(get_the_title()) );
       }
     }
   }
 endif;
 
 
-/**
- * Displays the article shares buttons
- *
- * @param string $type | choose links or share to get the link or to get the sharing action / print or email functions
- */
-if ( ! function_exists('modul_r_social_sharer') ) :
-	function modul_r_social_sharer($type = 'links') {
-
-  $opt_social_share_enabled = get_theme_mod('modul_r_social_share_enabled');
-  $opt_social_share_visibility = get_theme_mod('modul_r_social_share_visibility');
-
-  $wp_post_type = get_post_type();
-
-    if($opt_social_share_enabled == true):
-	    if ( $opt_social_share_visibility === 'all' ) {
-		    get_template_part( 'template-parts/fragments/content', 'social-share' );
-	    } else {
-		    if ( $wp_post_type === 'post' && $opt_social_share_visibility === 'posts' ) {
-			    get_template_part( 'template-parts/fragments/content', 'social-share' );
-		    } else if ( $wp_post_type === 'page' && $opt_social_share_visibility === 'pages' ) {
-			    get_template_part( 'template-parts/fragments/content', 'social-share' );
-		    }
-	    }
-	endif;
-	}
-endif;
-
-/**
- * Displays the related articles
- */
-if ( ! function_exists( 'modul_r_relateds' ) ) :
-	function modul_r_relateds() {
-
-		  $args = array(
-			  'post_type'      => 'post',
-			  'orderby'        => 'rand',
-			  'posts_per_page' => 3,
-		  );
-
-		  $query = new WP_Query( $args );
-
-		  if ( $query->have_posts() ) : ?>
-        <div class="relateds">
-
-          <h3><?php esc_html_e( 'You might be interested in...', 'modul-r' ); ?></h3>
-          <ul class="relateds-list">
-
-            <?php while ( $query->have_posts() ) : $query->the_post();
-              get_template_part( 'template-parts/content/content', 'related' );
-            endwhile; ?>
-
-          </ul>
-
-        </div>
-
-      <?php
-
-		  wp_reset_postdata();
-
-      endif;
-	}
-endif;
-
-// add 'has-featured-image' to body class if post has a featured image
 if ( ! function_exists('modul_r_custom_body_class') ) :
+	/**
+	 * Add 'has-featured-image' to body class if post has a featured image
+	 *
+	 * @param array $classes An array of classes that will be added to the body class.
+	 *
+	 * @return mixed
+	 */
 	function modul_r_custom_body_class( $classes ) {
 		global $post;
 		$woo_enabled = class_exists( 'WooCommerce' );
 
 		if (  is_page() || ( is_single() && !($woo_enabled && is_product()) ) || ( is_archive() && !($woo_enabled && is_product_category())) || ( ($woo_enabled && is_shop()) && get_theme_mod( 'modul_r_woo' ) ) ) {
-      // add the class "has-featured-image" if page or article and it ha a post thumbnail set
+      // Add the class "has-featured-image" if page or article and it ha a post thumbnail set.
       if ( has_post_thumbnail() ) {
         $classes[] = 'has-featured-image';
       }
     }
 
-    // get theme option "sidebar enabled"
+    // Get theme option "sidebar enabled".
     $opt_sidebar = get_theme_mod('modul_r_sidebar_enabled');
     if ( $opt_sidebar === true && ( ( is_archive() && ($woo_enabled && is_product_category()) ) || $woo_enabled && is_shop() || is_single() || (is_page() && !is_front_page()) ) ) {
         $classes[] = 'has-sidebar';
@@ -222,62 +63,3 @@ if ( ! function_exists('modul_r_custom_body_class') ) :
 	}
 endif;
 add_filter( 'body_class', 'modul_r_custom_body_class' );
-
-/**
- * Add a background to the headline if changed in the customizer
- */
-if ( ! function_exists('modul_r_header_image') ) :
-	function modul_r_header_image() {
-    $header_image = get_header_image();
-	  if ($header_image) {
-	    printf('<img src="%s" alt="%s" class="site-header-image" />', $header_image, get_bloginfo( 'title' ));
-    }
-	}
-endif;
-
-
-/**
- * Display the author of the post
- */
-if ( ! function_exists('modul_r_author') ) :
-	function modul_r_author() {
-
-	  global $post;
-
-	  ?>
-
-    <div class="article-metas">
-
-      <div class="article-author author vcard">
-        <div class="author-image">
-          <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>">
-            <span class="avatar u-photo"><?php echo get_avatar( get_the_author_meta( 'ID' ), '64', null, get_the_author() ); ?> </span>
-          </a>
-        </div>
-        <div class="author-details">
-          <a class="url" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>" rel="author">
-            <h3 class="name fn n"><?php the_author_meta( 'display_name' ); ?></h3>
-          </a>
-          <div class="author-description note">
-            <p><?php the_author_meta( 'description' ); ?></p>
-            <?php
-
-            $website = get_the_author_meta( 'url', $post->post_author );
-            if ( $website ) {echo '<a href="' . $website . '" class="u-url" rel="nofollow" target="_blank"><i class="social-ico www"></i></a>';}
-
-            $twitter = get_the_author_meta( 'twitter', $post->post_author );
-            if ( $twitter ) {echo '<a href="https://twitter.com/' . $twitter . '" class="u-url" rel="nofollow" target="_blank"><i class="social-ico twitter"></i></a>';}
-
-            $facebook = get_the_author_meta( 'facebook', $post->post_author );
-            if ( $facebook ) {echo '<a href="' . $facebook . '" class="u-url" rel="nofollow" target="_blank"><i class="social-ico facebook"></i></a>';}
-
-            ?>
-          </div>
-        </div>
-      </div>
-
-    </div>
-
-  <?php
-	}
-endif;
