a7bf3ecf1a798fa1a163b6ee90ae3200b9264baf
Erik Golinelli <erik@codekraft.it>
2023-02-12 13:53:45 +0100

    breadcrumbs component

---
+++ patterns/head-breadcrumbs.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Head Title Breadcrumbs
+ * Description: a pattern that shows the breadcrumbs using yoast breadcrumbs if available or using a custom function
+ * Slug: modul-r/head-title
+ * Categories: group, modul-r
+ */
+?>
+<?php
+modul_r_breadcrumbs();
