423f5cca8b9d1f8d666fc2b193e7136b33e4a743
Erik Golinelli <erik@codekraft.it>
2023-02-14 14:10:12 +0100

    better fitting for animated svg backgrounds

--- patterns/homepage-hero-animated.php
+++ patterns/homepage-hero-animated.php
@@ -4,7 +4,7 @@
  * Slug: modul-r/homepage-hero-animated
  * Categories: header, modul-r
  */
-$animated_background = '<!-- wp:html --><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin:auto;max-width: inherit;height: 100%;display:block;z-index:-1;position:absolute;top:0;left:0;right:0;" preserveAspectRatio="xMidYMid" viewBox="0 0 1920 1080">
+$animated_background = '<!-- wp:html --><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin:auto;max-width: inherit;min-width: 100%;height: 100%;display:block;z-index:-1;position:absolute;top:0;left:0;right:0;" preserveAspectRatio="none" viewBox="0 0 1920 1080">
 			<g transform="translate(960,540) scale(1,1) translate(-960,-540)">
 				<linearGradient id="line-gradient" x1="0" x2="1" y1="0" y2="0">
 					<stop stop-color="#6f4cad" offset="0"></stop>
@@ -25,22 +25,12 @@ $animated_background = '<!-- wp:html --><svg xmlns="http://www.w3.org/2000/svg" 
 							 calcMode="spline" keySplines="0.5 0 0.5 1;0.5 0 0.5 1;0.5 0 0.5 1" begin="-4s"
 							 values="M0 0L 0 899.146061943384Q 192 816.7807798398503  384 779.9528054585896T 768 773.6000722874003T 1152 638.2203937581166T 1536 560.5308580620235T 1920 516.9737119385375L 1920 0 Z;M0 0L 0 969.0958118391045Q 192 855.2958118102569  384 825.7967305011035T 768 807.4969238026301T 1152 612.2459601185939T 1536 694.2367039865289T 1920 424.394789142685L 1920 0 Z;M0 0L 0 888.7157015203587Q 192 915.1443819811288  384 882.6322372129603T 768 835.6347502554163T 1152 735.4500922675956T 1536 587.0794527772757T 1920 524.4642424098683L 1920 0 Z;M0 0L 0 899.146061943384Q 192 816.7807798398503  384 779.9528054585896T 768 773.6000722874003T 1152 638.2203937581166T 1536 560.5308580620235T 1920 516.9737119385375L 1920 0 Z"></animate>
 				</path>
-				<path d="" fill="url(#line-gradient)" opacity="0.4">
-					<animate attributeName="d" dur="10s" repeatCount="indefinite" keyTimes="0;0.333;0.667;1"
-							 calcMode="spline" keySplines="0.5 0 0.5 1;0.5 0 0.5 1;0.5 0 0.5 1" begin="-6s"
-							 values="M0 0L 0 1026.7767549545347Q 192 919.0596415677503  384 900.912570576953T 768 828.651113285109T 1152 673.772630598658T 1536 549.1392613666809T 1920 504.9265734105345L 1920 0 Z;M0 0L 0 987.1986911151462Q 192 816.6798793699361  384 782.9036300860288T 768 743.3303881130028T 1152 680.3120195354606T 1536 612.2444561265609T 1920 495.18140276109193L 1920 0 Z;M0 0L 0 929.201123812677Q 192 903.3750682316489  384 873.5125850921623T 768 768.8139592139507T 1152 642.9345298539894T 1536 593.3692042588123T 1920 408.7887722486921L 1920 0 Z;M0 0L 0 1026.7767549545347Q 192 919.0596415677503  384 900.912570576953T 768 828.651113285109T 1152 673.772630598658T 1536 549.1392613666809T 1920 504.9265734105345L 1920 0 Z"></animate>
-				</path>
-				<path d="" fill="url(#line-gradient)" opacity="0.4">
-					<animate attributeName="d" dur="10s" repeatCount="indefinite" keyTimes="0;0.333;0.667;1"
-							 calcMode="spline" keySplines="0.5 0 0.5 1;0.5 0 0.5 1;0.5 0 0.5 1" begin="-8s"
-							 values="M0 0L 0 927.757330415229Q 192 935.5036969367135  384 896.2927846551129T 768 767.0332596113809T 1152 610.0431016357014T 1536 527.2034111424723T 1920 356.9347500372055L 1920 0 Z;M0 0L 0 1036.6657127229266Q 192 969.5187642761874  384 927.8556095114153T 768 854.6825583489567T 1152 719.807429122534T 1536 522.4262978150718T 1920 540.3110955937918L 1920 0 Z;M0 0L 0 881.6604483792524Q 192 971.8896126881201  384 951.7214437333221T 768 755.6107004754474T 1152 770.5483481240326T 1536 621.3040998939546T 1920 414.8552996720887L 1920 0 Z;M0 0L 0 927.757330415229Q 192 935.5036969367135  384 896.2927846551129T 768 767.0332596113809T 1152 610.0431016357014T 1536 527.2034111424723T 1920 356.9347500372055L 1920 0 Z"></animate>
-				</path>
 			</g>
 		</svg><!-- /wp:html -->'
 ?>
-<!-- wp:cover {"useFeaturedImage":true,"overlayColor":"black","minHeight":100,"minHeightUnit":"vh","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|60"}}}} -->
+<!-- wp:cover {"useFeaturedImage":true,"overlayColor":"black","minHeight":80,"minHeightUnit":"vh","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|60"}}}} -->
 <div class="wp-block-cover"
-	 style="margin-top:0;margin-bottom:var(--wp--preset--spacing--60);min-height:100vh;">
+	 style="margin-top:0;margin-bottom:var(--wp--preset--spacing--60);min-height:80vh;">
 	<span aria-hidden="true"
 		  class="wp-block-cover__background has-black-background-color has-background-dim-100 has-background-dim"></span>
 	<div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"}} -->
