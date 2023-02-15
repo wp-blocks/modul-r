a7a052dcaac26b000d8c69ee28634a5aa4204ac0
Erik Golinelli <erik@codekraft.it>
2023-02-15 12:50:53 +0100

    fire all the theme script using a single event

--- src/scripts/scripts.ts
+++ src/scripts/scripts.ts
@@ -1,3 +1,23 @@
 // Modul-r scroll controller
-import './user/';
 import './user/scrollControl';
+
+/* Blaze slider */
+import 'blaze-slider/dist/blaze.css';
+import './user/slider.scss';
+
+/* Lightbox */
+import 'glightbox/dist/css/glightbox.min.css';
+import './user/lightbox.scss';
+
+import { modulrLightboxController } from './user/lightbox';
+import { modulrSliderController } from './user/slider';
+import { modulrScrollControl } from './user/scrollControl';
+
+document.addEventListener( 'DOMContentLoaded', () => {
+	/* enable lightboxes */
+	modulrLightboxController();
+	/* enable sliders */
+	modulrSliderController();
+	/* enable scroll animations */
+	modulrScrollControl();
+} );
