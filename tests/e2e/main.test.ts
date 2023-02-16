cde95550c2b71331eb3f643e478689127bad97f4
Erik Golinelli <erik@codekraft.it>
2023-02-16 15:51:42 +0100

    automatic homepage screenshot

--- tests/e2e/main.test.ts
+++ tests/e2e/main.test.ts
@@ -22,11 +22,21 @@ describe( 'Admin works!', () => {
 describe( 'Frontend works!', () => {
 	beforeAll( async () => {
 		await page.goto( 'http://localhost:8889/' );
-	}, 600000 );
+	} );
+
+	it( 'Should load properly front-facing website', async () => {
+		await page.setViewport( { height: 900, width: 1200 } );
+
+		// Take the screenshot of the page with puppeteer
+		const image = await page.screenshot( {
+			path: './screenshot.png',
+			fullPage: false,
+		} );
+	} );
 
 	it( 'Should load properly front-facing website', async () => {
 		// Assertions
 		const nodes = await page.$x( '//h1 /a[contains(text(), "modul-r")]' );
 		expect( nodes.length ).not.toEqual( 0 );
-	}, 60000 );
+	} );
 } );
