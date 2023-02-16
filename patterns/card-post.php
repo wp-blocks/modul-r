b714ba93750f12d3b790e9f93e0794e4ba4b3170
Erik Golinelli <erik@codekraft.it>
2023-02-16 22:29:16 +0100

    Post card pattern

---
+++ patterns/card-post.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Post card
+ * Slug: modul-r/post-card
+ * Categories: modul-r
+ */
+?>
+<!-- wp:post-template -->
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"is-style-card","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group alignwide is-style-card" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:post-featured-image {"isLink":true,"width":"100%","height":"max(15vw, 30vh)","align":"wide","className":"is-style-default"} /-->
+
+	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+		<div class="wp-block-group"><!-- wp:post-title {"isLink":true} /-->
+
+			<!-- wp:post-date {"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|gray"}}}}} /--></div>
+		<!-- /wp:group -->
+
+		<!-- wp:post-excerpt {"textColor":"gray"} /--></div>
+	<!-- /wp:group --></div>
+<!-- /wp:group -->
+<!-- /wp:post-template -->
+
