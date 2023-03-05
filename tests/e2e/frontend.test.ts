0565ea68732c65198505b6e5e57bcfe1799e134f
Erik Golinelli <erik@codekraft.it>
2023-03-05 21:31:16 +0100

    test e2e update

--- tests/e2e/frontend.test.ts
+++ tests/e2e/frontend.test.ts
@@ -1,28 +1,11 @@
 /**
  * Load utilities from the e2e-test-utils package.
  */
-// Load utilities from the e2e-test-utils package.
-import { visitAdminPage } from '@wordpress/e2e-test-utils';
-
-// Name of the test suite.
-describe( 'Admin works!', () => {
-	it( 'Should load properly admin', async () => {
-		// Navigate the admin and performs tasks
-		// Use Puppeteer APIs to interact with mouse, keyboard...
-		await visitAdminPage( '/' );
-
-		// Assertions
-		const nodes = await page.$x(
-			'//h2[contains(text(), "Welcome to WordPress!")]'
-		);
-		expect( nodes.length ).not.toEqual( 0 );
-	}, 60000 );
-} );
 
 describe( 'Frontend works!', () => {
 	beforeAll( async () => {
 		await page.goto( 'http://localhost:8889/' );
-	}, 10000 );
+	}, 15000 );
 
 	it( 'Should take the screenshot of the homepage', async () => {
 		await page.setViewport( { height: 900, width: 1200 } );
@@ -32,6 +15,8 @@ describe( 'Frontend works!', () => {
 			path: './screenshot.png',
 			fullPage: false,
 		} );
+
+		expect( image ).toBeTruthy();
 	} );
 
 	it( 'Should load properly front-facing website', async () => {
