6a88166cf1cb1d0310d4598fb3e5476f2b1694ee
Erik Golinelli <erik@codekraft.it>
2023-03-05 16:29:02 +0100

    website hero pattern

--- patterns/website-hero.php
+++ patterns/website-hero.php
@@ -1,26 +1,23 @@
 <?php
 /**
- * Title: Page Hero
+ * Title: WebSite Hero
  * Slug: modul-r/page-hero
  * Categories: header, modul-r
  */
 ?>
-<!-- wp:cover {"useFeaturedImage":true,"overlayColor":"black","minHeight":50,"minHeightUnit":"vh","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|60"}}}} -->
+<!-- wp:cover {"useFeaturedImage":true,"overlayColor":"black","minHeight":75,"minHeightUnit":"vh","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|60"}}}} -->
 <div class="wp-block-cover"
-	 style="margin-top:0;margin-bottom:var(--wp--preset--spacing--60);min-height:50vh">
+	 style="margin-top:0;margin-bottom:var(--wp--preset--spacing--60);min-height:75vh">
 	<span aria-hidden="true"
 		  class="wp-block-cover__background has-black-background-color has-background-dim-100 has-background-dim"></span>
 	<div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"}} -->
 		<div class="wp-block-group">
 			<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"},"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-			<div class="wp-block-group"
-				 style="padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">
+			<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">
 				<!-- wp:site-title {"textAlign":"center","isLink":false,"style":{"typography":{"fontSize":"5rem"}},"textColor":"white"} /-->
 
 				<!-- wp:paragraph {"align":"center","textColor":"primary"} -->
-				<p class="has-text-align-center has-primary-color has-text-color">Lorem ipsum dolor
-					sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-					aliqua.</p>
+				<p class="has-text-align-center has-primary-color has-text-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 				<!-- /wp:paragraph -->
 
 			</div><!-- /wp:group -->
