1516adedaa33202e6d2eb0ae2e891572698bb7e7
Erik Golinelli <erik@codekraft.it>
2023-03-05 16:24:00 +0100

    homepage

--- patterns/homepage-hero.php
+++ patterns/homepage-hero.php
@@ -5,23 +5,32 @@
  * Categories: header, modul-r
  */
 ?>
-<!-- wp:cover {"useFeaturedImage":true,"overlayColor":"black","minHeight":70,"minHeightUnit":"vh","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|60"}}}} -->
-<div class="wp-block-cover"
-	 style="margin-top:0;margin-bottom:var(--wp--preset--spacing--60);min-height:70vh">
-	<span aria-hidden="true"
-		  class="wp-block-cover__background has-black-background-color has-background-dim-100 has-background-dim"></span>
+<!-- wp:cover {"useFeaturedImage":true,"overlayColor":"black","minHeight":498,"minHeightUnit":"px","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|60"}}}} -->
+<div class="wp-block-cover" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--60);min-height:498px"><span
+			aria-hidden="true"
+			class="wp-block-cover__background has-black-background-color has-background-dim-100 has-background-dim"></span>
 	<div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"}} -->
 		<div class="wp-block-group">
-			<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|70","left":"var:preset|spacing|60"},"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-			<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--60)"></div>
-
-			<!-- wp:site-title {"textAlign":"center","isLink":false,"style":{"typography":{"fontSize":"5rem"}},"textColor":"white"} /-->
+			<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"},"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group"
+				 style="padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">
+				<!-- wp:site-title {"textAlign":"center","isLink":false,"style":{"typography":{"fontSize":"5rem"}},"textColor":"white"} /-->
 
 				<!-- wp:paragraph {"align":"center","textColor":"primary"} -->
-				<p class="has-text-align-center has-primary-color has-text-color">Lorem ipsum dolor
-					sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-					aliqua.</p>
-				<!-- /wp:paragraph -->
+				<p class="has-text-align-center has-primary-color has-text-color">Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				<!-- /wp:paragraph --></div>
+
+				<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+				<div class="wp-block-buttons"><!-- wp:button -->
+					<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">QUICK START 🡲</a></div>
+					<!-- /wp:button -->
+
+					<!-- wp:button {"textColor":"white","className":"is-style-outline"} -->
+					<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-white-color has-text-color wp-element-button">DOWNLOAD 🡲</a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons -->
+
 			<!-- /wp:group --></div>
 		<!-- /wp:group --></div>
 </div>
