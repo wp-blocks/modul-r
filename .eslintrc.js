6254e445118a6ddf8cbbff08991990eeb1ca6459
Erik Golinelli <erik@codekraft.it>
2023-03-06 13:46:45 +0100

    eslint configuration update @johnhooks - do you think the default configuration is well suited for typescript? I think this one could improve something, maybe we can use it in the wp-blocks env to extend the default configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,43 @@ eslintConfig.parserOptions = {
 			'@babel/preset-typescript',
 		],
 	},
+	rules: {
+		'arrow-body-style': [ 'error', 'as-needed' ],
+		// TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
+		'default-case': 'off',
+		// 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/291)
+		'no-dupe-class-members': 'off',
+		// 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/477)
+		'no-undef': 'off',
+		'@typescript-eslint/no-use-before-define': [
+			'warn',
+			{
+				functions: false,
+				classes: false,
+				variables: false,
+				typedefs: false,
+			},
+		],
+		'no-unused-expressions': 'off',
+		'@typescript-eslint/no-unused-expressions': [
+			'error',
+			{
+				allowShortCircuit: true,
+				allowTernary: true,
+				allowTaggedTemplates: true,
+			},
+		],
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{
+				args: 'none',
+				ignoreRestSiblings: true,
+			},
+		],
+		'no-useless-constructor': 'off',
+		'@typescript-eslint/no-useless-constructor': 'warn',
+	},
 };
 
 module.exports = eslintConfig;
