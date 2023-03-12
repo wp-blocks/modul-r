7b2f2a059a2e02e9c7b15062bb528fb532ec9e2e
Erik Golinelli <erik@codekraft.it>
2023-03-12 14:43:37 +0100

    more general content for patterns

--- patterns/media-and-bold-text.php
+++ patterns/media-and-bold-text.php
@@ -11,11 +11,11 @@ $image_src = esc_url( get_theme_file_uri( 'img/demo/wapuu-original.avif' ) );
 <!-- wp:media-text {"mediaId":134,"mediaLink":"<?php echo $image_src; ?>","mediaType":"image","mediaWidth":39,"imageFill":false} -->
 <div class="wp-block-media-text alignwide is-stacked-on-mobile" style="grid-template-columns:39% auto"><figure class="wp-block-media-text__media"><img src="<?php echo $image_src; ?>" alt="" class="wp-image-134 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
 		<div class="wp-block-group"><!-- wp:heading {"level":1,"style":{"typography":{"fontSize":"8rem"}},"textColor":"secondary","className":"is-style-default"} -->
-			<h1 class="is-style-default has-secondary-color has-text-color" style="font-size:8rem">Install NodeJS</h1>
+			<h1 class="is-style-default has-secondary-color has-text-color" style="font-size:8rem">Big Wapu Title</h1>
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph -->
-			<p>First you need to download and install&nbsp;<strong>NodeJS</strong>&nbsp;from the link below:</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <b>Cupiditate iusto magni</b> minus odio perferendis quidem repellat similique tempora temporibus voluptates. Ad quas, voluptatibus! Animi commodi esse nulla odit quasi quibusdam?</p>
 			<!-- /wp:paragraph -->
 
 			<!-- wp:buttons {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}}} -->
