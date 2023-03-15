c5699f1f039fb86005c6b4d11a02ec46b1b82845
Erik Golinelli <erik@codekraft.it>
2023-03-15 23:28:11 +0100

    probably isn't no longer required to fix the view in ios in this way ```js let vh = window.innerHeight * 0.01; document.documentElement.style.setProperty('--vh', `${vh}px`); ```

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -17,31 +17,6 @@ if ( ! function_exists( 'modul_r_theme_scripts' ) ) :
 endif;
 add_action( 'wp_enqueue_scripts', 'modul_r_theme_scripts' ); // Add Theme admin scripts
 
-if ( ! function_exists( 'modul_r_content_height_fix' ) ) :
-	/**
-	 * Fix for ios that overlaps content with the lower nav bar
-	 */
-	function modul_r_content_height_fix() {
-		?>
-		<script>
-			function setFullHeight() {
-				// First we get the viewport height, and we multiply it by 1% to get a value for a vh unit
-				let vh = window.innerHeight * 0.01;
-				// Then we set the value in the --vh custom property to the root of the document
-				document.documentElement.style.setProperty('--vh', `${vh}px`);
-			}
-
-			setFullHeight();
-
-			window.addEventListener('resize', function () {
-				setFullHeight();
-			});
-		</script>
-		<?php
-	}
-endif;
-add_action( 'wp_head', 'modul_r_content_height_fix', 10 );
-
 if ( ! function_exists( 'modul_r_theme_drawer_color' ) ) :
 	/**
 	 * Add color styling from theme
