1ca5312f7cf761c4ab70f9d9e9cca16c6d831f0c
Erik Golinelli <erik@codekraft.it>
2026-05-01 18:02:28 +0200

    woo

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -1,7 +1,7 @@
 <?php
 if ( class_exists( 'WooCommerce' ) ) {
 	/* Adding support for the WooCommerce product gallery features. */
-	add_theme_support( 'wc-product-gallery-zoom' );
+	remove_theme_support( 'wc-product-gallery-zoom' );
 	add_theme_support( 'wc-product-gallery-lightbox' );
 	add_theme_support( 'wc-product-gallery-slider' );
 }
