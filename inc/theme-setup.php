58a2d3f290f1fdab99d7df4a75ff17c910d658ad
Erik Golinelli <erik@codekraft.it>
2026-05-01 18:02:10 +0200

    theme base setup

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -1,18 +1,12 @@
 <?php
 if ( ! function_exists( 'modul_r_theme_setup' ) ) :
 	function modul_r_theme_setup() {
-		/*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		*/
-		load_theme_textdomain( 'modul-r', get_template_directory() . '/languages' );
-
-		/*
+		/**
 		 * Add theme support for menus.
 		 */
 		add_theme_support( 'menus' );
 
-		/*
+		/**
 		* Let WordPress manage the document title.
 		* By adding theme support, we declare that this theme does not use a
 		* hard-coded <title> tag in the document head, and expect WordPress to
@@ -21,13 +15,6 @@ if ( ! function_exists( 'modul_r_theme_setup' ) ) :
 		add_theme_support( 'title-tag' );
 
 		add_theme_support( 'admin-bar', array( 'callback' => '__return_false' ) );
-
-		// This theme uses wp_nav_menu() in Primary Navigation.
-		register_nav_menus(
-			array(
-				'main-menu' => esc_html__( 'Primary', 'modul-r' ),
-			)
-		);
 	}
 endif;
 add_action( 'after_setup_theme', 'modul_r_theme_setup' );
@@ -43,3 +30,12 @@ if ( ! function_exists( 'modul_r_comments' ) ) :
 		comments_template();
 	}
 endif;
+
+function modulr_i18n() {
+	/**
+	* Make theme available for translation.
+	* Translations can be filed in the /languages/ directory.
+	*/
+	load_theme_textdomain( 'modul-r', get_template_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'modulr_i18n' );
