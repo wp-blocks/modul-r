5bde9e0554aae79bc80b8c65c1de16790cd2d5a3
Erik Golinelli <erik@codekraft.it>
2026-05-16 18:21:34 +0200

    chore: replace hero-tight with headline patterns across templates; update headline logic and redesign headline.php for improved modularity and consistency

--- patterns/headline.php
+++ patterns/headline.php
@@ -4,26 +4,17 @@
  * Slug: modul-r/headline
  * Categories: headlines, modul-r
  */
-
-$headline = '
-	<!-- wp:post-title {"level":1,"className":"is-style-gradient","style":{"typography":{"fontStyle":"normal","fontWeight":"300","textTransform":"uppercase"}},"fontSize":"xxx-large"} /-->
-	<!-- wp:pattern {"slug":"modul-r/headline-meta"} /-->
-';
-if ( is_search() ) {
-	$headline = '<!-- wp:query-title {"type":"search","textAlign":"center","showSearchTerm":false,"align":"wide","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background","fontSize":"xx-large"} /-->';
-} else if (is_archive()) {
-	$headline = '<!-- wp:query-title {"type":"archive","showPrefix":false,"level":1,"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"className":"hero-title is-style-gradient","fontSize":"xxx-large"} /-->';
-} else if (is_shop() || is_product_category() || is_product_tag()) {
-	$headline = '<!-- wp:query-title {"type":"archive","textAlign":"center","showPrefix":false,"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontSize":"4rem"}},"textColor":"background"} /-->';
-} else if ( is_front_page() ) {
-	$headline = '
-		<!-- wp:query-title {"level":1,"fontSize":"xxx-large"} /-->
-		<!-- wp:pattern {"slug":"modul-r/headline-meta"} /-->
-	';
-}
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|50"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--50)">
-	<?php echo $headline; ?>
+<!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"overlayColor":"primary","isUserOverlayColor":true,"minHeight":33,"minHeightUnit":"vh","isDark":false,"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;min-height:33vh">
+	<span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim"></span>
+	<div class="wp-block-cover__inner-container">
+		<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+		<div class="wp-block-group">
+			<!-- wp:post-title {"level":1,"className":"is-style-default","style":{"typography":{"fontStyle":"normal","fontWeight":"300","textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background","fontSize":"xxx-large"} /-->
+			<!-- wp:pattern {"slug":"modul-r/article-meta"} /-->
+		</div>
+		<!-- /wp:group -->
+	</div>
 </div>
-<!-- /wp:group -->
+<!-- /wp:cover -->
