1870c73109662fe9c16d0a95b1a30a89cad9a20a
Erik Golinelli <erik@codekraft.it>
2026-05-16 20:03:44 +0200

    chore: remove unused modul_r_comments function for cleanup and improved code maintainability

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -19,18 +19,6 @@ if ( ! function_exists( 'modul_r_theme_setup' ) ) :
 endif;
 add_action( 'after_setup_theme', 'modul_r_theme_setup' );
 
-if ( ! function_exists( 'modul_r_comments' ) ) :
-	/**
-	 * Displays the comments template
-	 *
-	 * @return void
-	 */
-	function modul_r_comments() {
-		// If comments are open, or we have at least one comment, load up the comment template.
-		comments_template();
-	}
-endif;
-
 function modulr_i18n() {
 	/**
 	* Make theme available for translation.
