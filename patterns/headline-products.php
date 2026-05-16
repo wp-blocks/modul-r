77201e79519fcbb3b0687fa8c9bbcaf87330a23b
Erik Golinelli <erik@codekraft.it>
2026-05-16 18:20:57 +0200

    chore: replace hero-tight with new headline patterns; add headline-products, headline-simple, headline-archive, and headline-search for improved modularity and layout flexibility

--- patterns/headline-products.php
+++ patterns/headline-products.php
@@ -1,16 +1,19 @@
 <?php
 /**
- * Title: Hero Tight
- * Slug: modul-r/hero-tight
+ * Title: Headline Products
+ * Slug: modul-r/headline-products
  * Categories: headlines, modul-r
  */
-
 ?>
 <!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"overlayColor":"primary","isUserOverlayColor":true,"minHeight":33,"minHeightUnit":"vh","isDark":false,"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-cover is-light" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;min-height:33vh">
 	<span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim"></span>
 	<div class="wp-block-cover__inner-container">
-		<!-- wp:pattern {"slug":"modul-r/headline"} /-->
+		<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+		<div class="wp-block-group">
+			<!-- wp:query-title {"type":"archive","textAlign":"center","showPrefix":false,"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontSize":"6rem","textTransform":"uppercase","fontStyle":"normal","fontWeight":"300"}},"textColor":"background","fontFamily":"default"} /-->
+		</div>
+		<!-- /wp:group -->
 	</div>
 </div>
 <!-- /wp:cover -->
