d4decb020cd3b45e9ea4f7d36702506b4e14243a
Erik Golinelli <erik@codekraft.it>
2026-05-16 20:04:24 +0200

    chore: update headline and headline-products patterns; adjust dimensions, typography, and styles for improved layout consistency

--- patterns/headline-products.php
+++ patterns/headline-products.php
@@ -5,13 +5,13 @@
  * Categories: headlines, modul-r
  */
 ?>
-<!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"overlayColor":"primary","isUserOverlayColor":true,"minHeight":33,"minHeightUnit":"vh","isDark":false,"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-cover is-light" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;min-height:33vh">
+<!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"overlayColor":"primary","isUserOverlayColor":true,"minHeight":40,"minHeightUnit":"vh","isDark":false,"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;min-height:40vh">
 	<span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim"></span>
 	<div class="wp-block-cover__inner-container">
 		<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
 		<div class="wp-block-group">
-			<!-- wp:query-title {"type":"archive","textAlign":"center","showPrefix":false,"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontSize":"6rem","textTransform":"uppercase","fontStyle":"normal","fontWeight":"300"}},"textColor":"background","fontFamily":"default"} /-->
+			<!-- wp:query-title {"type":"archive","textAlign":"center","showPrefix":false,"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontSize":"5rem","textTransform":"uppercase","fontStyle":"normal","fontWeight":"300"}},"textColor":"background","fontFamily":"default"} /-->
 		</div>
 		<!-- /wp:group -->
 	</div>
--- patterns/headline.php
+++ patterns/headline.php
@@ -5,13 +5,13 @@
  * Categories: headlines, modul-r
  */
 ?>
-<!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"overlayColor":"primary","isUserOverlayColor":true,"minHeight":33,"minHeightUnit":"vh","isDark":false,"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-cover is-light" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;min-height:33vh">
+<!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"overlayColor":"primary","isUserOverlayColor":true,"minHeight":50,"minHeightUnit":"vh","isDark":false,"className":"is-style-wave-clip","style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light is-style-wave-clip" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;min-height:50vh">
 	<span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim"></span>
 	<div class="wp-block-cover__inner-container">
 		<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
 		<div class="wp-block-group">
-			<!-- wp:post-title {"level":1,"className":"is-style-default","style":{"typography":{"fontStyle":"normal","fontWeight":"300","textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background","fontSize":"xxx-large"} /-->
+			<!-- wp:post-title {"textAlign":"center","level":1,"className":"is-style-default","style":{"typography":{"fontStyle":"normal","fontWeight":"300","lineHeight":"1.2","textTransform":"uppercase","fontSize":"6rem"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background"} /-->
 			<!-- wp:pattern {"slug":"modul-r/article-meta"} /-->
 		</div>
 		<!-- /wp:group -->
